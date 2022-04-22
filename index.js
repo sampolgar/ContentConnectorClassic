eed81d0b31622bf0ac6c81d91e1e31c5237d9bfd
SamuelPolgar <sampolgar@gmail.com>
2022-04-22 16:56:19 +1000

    add promises to api calls

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const res = require('express/lib/response');
 const images = require('./images.json')
+const fs = require('fs')
 
 const app = express();
 const PORT = 3000;
@@ -15,7 +15,7 @@ app.get('/images', (req, res) => {
     res.json(images)
 });
 
-app.get('/images/:id', req, res) => {
+app.get('/images/:id', (req, res) => {
     const imgId = Number(req.params.id);
     const getImg = images.find((img) => img.id === imgId);
 
@@ -24,7 +24,31 @@ app.get('/images/:id', req, res) => {
     } else {
         res.json(getImg)
     }
+});
+
+app.post('/images', (req, res) => {
+    let getImageData = new Promise(function(response, rejected){
+        console.log('here'+JSON.stringify(req.body))
+        response(testPost())
+    })
+
+    getImageData
+    .then(function(success){
+        res.send('Hello: '+JSON.stringify(req.body));
+    }, function(error){
+        console.log(error)
+    })
+})
+
+function testPost(){
+    console.log('jaldkfjklajdflksjalksdf')
+    const incomingImg = {
+        "id": 5,
+        "name": "image5",
+        "link": "https:1231234.com"
+    };
 }
+
 
 app.post('/oauth2/token/', (req, res) => {
     res.send('Hello: '+JSON.stringify(req.body));
@@ -33,4 +57,3 @@ app.post('/oauth2/token/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Express server currently running on port ${PORT}`)
 });
-// [label index.js]
